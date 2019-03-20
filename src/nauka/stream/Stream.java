07ef0c18bf9b6fec5214daab00c3f2da295e36ce
MichalKrzyzak <michalkrzyzak95@gmail.com>
2019-03-20 15:16:32 +0100

    Added sorting option

--- src/nauka/stream/Stream.java
+++ src/nauka/stream/Stream.java
@@ -1,10 +1,11 @@
 package nauka.stream;
 
+import java.util.Comparator;
 import java.util.List;
 
 class Stream {
 
-    void filterStudents(List<Student> studentList){
+    void filterStudents(List<Student> studentList) {
 
         System.out.println("First Stream...");
         studentList.stream()
@@ -12,13 +13,30 @@ class Stream {
                 .map(student -> (new StringBuilder(student.getFirstName()).append(" ").append(student.getLastName()).toString()))
                 .forEach(System.out::println);
 
-        System.out.println("Second Stream...");
+        System.out.println("\nSecond Stream...");
         long count = studentList.stream()
-                .filter(student -> student.getHeight()>1.7)
+                .filter(student -> student.getHeight() > 1.7)
                 .map(student -> (new StringBuilder(student.getFirstName()).append(" ").append(student.getLastName()).toString()))
                 .peek(student -> System.out.println(student))
                 .count();
-
         System.out.println(count);
+
+        System.out.println("\nThird Stream...");
+        studentList.stream()
+                .sorted(Comparator.comparing(Student::getLastName))
+                .map(student -> (new StringBuilder(student.getFirstName()).append(" ").append(student.getLastName()).toString()))
+                .forEachOrdered(System.out::println);
+
+        System.out.println("\nFourth Stream...");
+        studentList.stream()
+                .sorted(Comparator.comparing(Student::getFirstName))
+                .map(student -> (new StringBuilder(student.getFirstName()).append(" ").append(student.getLastName()).toString()))
+                .forEachOrdered(System.out::println);
+
+        System.out.println("\nFifth Stream...");
+        studentList.stream()
+                .sorted(Comparator.comparing(Student::getHeight))
+                .map(student -> (new StringBuilder(student.getFirstName()).append(" ").append(student.getLastName()).toString()))
+                .forEachOrdered(System.out::println);
     }
 }
--- src/nauka/stream/Students.java
+++ src/nauka/stream/Students.java
@@ -3,9 +3,9 @@ package nauka.stream;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Students {
+public class Students{
 
-    public void filterStudents(){
+    public void filterStudents() {
         Stream stream = new Stream();
         stream.filterStudents(getStudents());
     }
@@ -34,9 +34,56 @@ public class Students {
                 .height(1.68)
                 .build();
 
+        Student student4 = new Student.Builder()
+                .firstName("Janina")
+                .lastName("Kanał")
+                .gender("K")
+                .height(1.62)
+                .build();
+
+        Student student5 = new Student.Builder()
+                .firstName("Krzysztof")
+                .lastName("Wypływ")
+                .gender("M")
+                .height(2.1)
+                .build();
+
+        Student student6 = new Student.Builder()
+                .firstName("Adrian")
+                .lastName("Anatowski")
+                .gender("M")
+                .height(1.79)
+                .build();
+
+        Student student7 = new Student.Builder()
+                .firstName("Zbigniew")
+                .lastName("Małecki")
+                .gender("M")
+                .height(1.82)
+                .build();
+
+        Student student8 = new Student.Builder()
+                .firstName("Dominik")
+                .lastName("Chmiel")
+                .gender("M")
+                .height(1.81)
+                .build();
+
+        Student student9 = new Student.Builder()
+                .firstName("Daria")
+                .lastName("Wanatowicz")
+                .gender("K")
+                .build();
+
         studentsList.add(student1);
         studentsList.add(student2);
         studentsList.add(student3);
+        studentsList.add(student4);
+        studentsList.add(student5);
+        studentsList.add(student6);
+        studentsList.add(student7);
+        studentsList.add(student8);
+        studentsList.add(student9);
 
         return studentsList;
     }
